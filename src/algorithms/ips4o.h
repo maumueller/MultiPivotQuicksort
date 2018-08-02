2c33beb40ea6eaac79f0ec345e79bbe86aa865f4
Martin Aumueller <maau@itu.dk>
2018-08-02 14:46:09 +0200

    Added ips4o.

---
+++ src/algorithms/ips4o.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * src/algorithms/stdsort.h
+ *
+ * Sorting using std::sort
+ *
+ ******************************************************************************
+ * Copyright (C) 2014 Timo Bingmann <tb@panthema.net>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#include <algorithm>
+#include "ips4o/ips4o.hpp"
+
+namespace ips4o {
+
+template <typename ValueType>
+void sort1()
+{
+    assert( g_input_size % sizeof(ValueType) == 0 );
+
+    ValueType* input = (ValueType*)g_input;
+    size_t n = g_input_size / sizeof(ValueType);
+
+    ips4o::sort(input, input + n);
+}
+
+CONTESTANT_REGISTER_ALL(sort1, "ips4o", "ips4o")
+
+} // namespace stdsort
